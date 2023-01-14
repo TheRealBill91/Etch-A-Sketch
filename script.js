3141d609e1821ef0d1693a7f1a86b702c5c1e6e1
Billy <bc35786@gmail.com>
2023-01-14 13:08:15 -0600

    Make 16x16 grid smaller overall

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let divSquares;
 let gridBorder = document.querySelector('.gridBorder');
 
-function createDivSquares() {
+//Creates 16x16 grid
+function createFirstGrid() {
     for (let i = 0; i < 256; i++) {
         divSquares = document.createElement('div');
         divSquares.classList.add('divSquare');
