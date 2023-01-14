219ec20a2e8879f11fe45130f71765bf300d96a8
Billy <bc35786@gmail.com>
2023-01-14 13:02:49 -0600

    Make first grid 16x16 instead of 15x17

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let divSquares;
 let gridBorder = document.querySelector('.gridBorder');
 
 function createDivSquares() {
-    for (let i = 0; i < 255; i++) {
+    for (let i = 0; i < 256; i++) {
         divSquares = document.createElement('div');
         divSquares.classList.add('divSquare');
         gridBorder.appendChild(divSquares);
